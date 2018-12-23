e1bfd359f8e05b5bec4f1097608eef94f9e2a5ad
omniass <oomniaa.2020@gmail.com>
2018-12-23 19:29:28 +0200

    "SignIn" Searches in database

--- SQLCLients/class.UsersSQLClient.php
+++ SQLCLients/class.UsersSQLClient.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once("SQLClients/class.SQLClient.php");
+
 require_once("classes/class.User.php");
+
 require_once("classes/class.Category.php");
 
 class UsersSQLClient extends SQLClient
@@ -13,17 +15,26 @@ class UsersSQLClient extends SQLClient
 
     public function deleteUserAccount($userId)
     {
-        $this -> db -> query("DELETE FROM `users` WHERE id = $userId");
+        $this -> db -> query("DELETE FROM users WHERE id = $userId");
     }
 
-    public function getUserByEmail($email)
+    public function getUserBy_Email_Pass($email, $pass)
     {
+        $query = "SELECT * FROM users WHERE email = '$email' AND password='$pass'";
+        $result = ($this -> db -> query($query));
+        $result = $result -> fetch_assoc(); 
+
+        if($result)
+        {
+            return new User($result["userId"], $result["fullName"], $result["email"], $result["phoneNumber"], $result["imgUrl"]);
+        }
         
+        return null;
     }
 
     public function getUserById($userId)
     {
-        $query = "SELECT * FROM `users` WHERE id = $userId";
+        $query = "SELECT * FROM users WHERE id = $userId";
         $result = ($this -> db -> query($query));
         $result = $result -> fetch_assoc();
 
@@ -38,7 +49,7 @@ class UsersSQLClient extends SQLClient
     public function getUsersByIds($ids)
     {
         if(count($ids)){
-            $query = "SELECT * FROM `users` WHERE id = " . $ids[0];
+            $query = "SELECT * FROM users WHERE id = " . $ids[0];
             for ($i = 1; $i < count($ids); $i++) { 
                 $query = $query . " or id = " . $ids[i];
             }
